69ae78f041b019e7ab2c1432b38be95f8efe7e22
Szekely Reka <szekely.reka.ext@kesz.hu>
2026-02-19 11:09:35 +0200

    PizzaBuilderBase introduced as base class

--- PizzaBuilder/Builders/ItalianBuilder.cs
+++ PizzaBuilder/Builders/ItalianBuilder.cs
@@ -2,60 +2,15 @@
 
 namespace PizzaBuilder.Builders
 {
-    internal class ItalianBuilder : IBuilder
+    internal class ItalianBuilder : PizzaBuilderBase
     {
-        public string size;
-        public string dough;
-        public string sauce;
-        public List<string> Toppings = new();
-        public bool ExtraCheese;
-
-        public IBuilder SetSize(string size)
+        public override Pizza Build()
         {
-            this.size = size;
-            return this;
-        }
-
-        public IBuilder SetDough(string dough)
-        {
-            this.dough = dough;
-            return this;
-        }
-
-        public IBuilder SetSauce(string sauce)
-        {
-            this.sauce = sauce;
-            return this;
-        }
-
-        public IBuilder AddTopping(params string[] toppings)
-        {
-            foreach (string topping in toppings)
-            {
-                Toppings.Add(topping);
-            }
-            return this;
-        }
-
-        public IBuilder AddExtraCheese(bool extraCheese)
-        {
-            ExtraCheese = extraCheese;
-            return this;
-        }
-
-        public Pizza Build()
-        {
-            if (Toppings.Count > 10 && size == "Small")
+            if (toppings.Count > 10 && size == "Small")
                 throw new Exception("Too many topppings for a small pizza");
-            Pizza pizza = new(size, dough, sauce, Toppings, ExtraCheese);
+            Pizza pizza = new(size, dough, sauce, toppings, extraCheese);
             Reset();
             return pizza;
-        }
-
-        public void Reset()
-        {
-            Toppings = new();
-            ExtraCheese = false;
         }
     }
 }
---
+++ PizzaBuilder/Builders/PizzaBuilderBase.cs
@@ -0,0 +1,52 @@
+﻿using PizzaBuilder.Product;
+
+namespace PizzaBuilder.Builders
+{
+    public abstract class PizzaBuilderBase : IBuilder
+    {
+        protected string size = "Medium";
+        protected string dough = "Thin";
+        protected string sauce = "Tomato";
+        protected List<string> toppings = new();
+        protected bool extraCheese = false;
+
+        public IBuilder SetSize(string size)
+        {
+            this.size = size;
+            return this;
+        }
+
+        public IBuilder SetDough(string dough)
+        {
+            this.dough = dough;
+            return this;
+        }
+
+        public IBuilder SetSauce(string sauce)
+        {
+            this.sauce = sauce;
+            return this;
+        }
+
+        public IBuilder AddTopping(params string[] toppings)
+        {
+            this.toppings.Append(toppings.ToString());
+            return this;
+        }
+
+        public IBuilder AddExtraCheese(bool extraCheese)
+        {
+            this.extraCheese = extraCheese;
+            return this;
+        }
+
+        public abstract Pizza Build();
+
+
+        public void Reset()
+        {
+            toppings.Clear();
+            extraCheese = false;
+        }
+    }
+}
